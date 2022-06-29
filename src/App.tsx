e92aee01c996e5940b96ec0d8a83ff84dcf98700
Bruno Mistro <34082572+brunomistro@users.noreply.github.com>
2022-06-29 16:42:30 -0300

    components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import Formulario from "./components/Formulario";
+import Lista from "./components/Lista";
+
 export default function App() {
   return (
     <div className="App">
+			<Formulario />
+			<Lista />
     </div>
   );
 }
---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function index() {
+	return (
+		<button>Botão</button>
+	)
+}
